9af22b1df767f96077a6ae7dc69a33945bbc636d
Jayme Klein <jaymerebula04@gmail.com>
2023-03-01 08:37:52 -0300

    criaçao do arquivo pessoa

---
+++ pessoa.py
@@ -0,0 +1,36 @@
+class Pessoa:
+
+    data_nascimento: str = ''
+    nome: str = ''
+    cpf: str = ''
+
+    def __init__(self, data_nascimento, nome, cpf) -> None:
+        self.data_nascimento = data_nascimento
+        self.nome = nome
+        self.cpf = cpf
+        pass
+
+class Funcionario(Pessoa):
+
+    data_admissao: str = ''
+    salario: float = 0.0
+
+    def __init__(self, data_nascimento, nome, cpf, data_admissao, salario) -> None:
+        super().__init__(data_nascimento, nome, cpf)
+
+        self.data_admissao = data_admissao
+        self.salario = salario
+
+class Cliente(Pessoa):
+
+    ultima_visita: str = ''
+    saldo: float = 0.0
+
+    def __init__(self, data_nascimento, nome, cpf, ultima_visita, saldo) -> None:
+        super().__init__(data_nascimento, nome, cpf)
+
+        self.ultima_visita = ultima_visita
+        self.saldo = saldo
+
+pass
+    
